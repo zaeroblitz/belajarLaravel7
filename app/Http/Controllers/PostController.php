ee8f86c3116e57c04169143bc09b29cd1150b1ab
Zaero Blitz <zaeroblitz@gmail.com>
2021-04-10 12:54:00 +0700

    Add Post Model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,7 +10,7 @@ class PostController extends Controller
 {
     public function show($slug)
     {
-        $post = DB::table('posts')->where('slug', $slug)->first();
+        $post = Post::where('slug', $slug)->firstOrFail();
         // dd($posts);
         return view('posts.show', compact('slug', 'post'));
     }
