f74aa830e2fe8d8d0a01029ff88638ab921e895a
Zaero Blitz <zaeroblitz@gmail.com>
2021-04-11 22:07:57 +0700

    Eloquent Workflow

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function scopeLatestFirst()
+    {
+        return $this->latest()->first();
+    }
+
+    public function scopeOrderByTitle()
+    {
+        return $this->orderBy('title', 'asc')->get();
+    }
 }
